55eeac675c4546cf7b6295179aa38d99da6daff8
SachinMhz <sachin.mhz.sm@gmail.com>
2020-06-19 22:46:55 +0545

    complete section3

---
+++ Leapfrog/design_final_project/todo.py
@@ -0,0 +1,31 @@
+all:
+    padding left right 15 in all container
+
+
+header
+    header img: 
+        wrap in a tag 
+        responsive width
+            decrease size of header icon
+            remove tag-line
+            
+        ham-burger menu use button and wrap in a tag
+
+    nav
+        use specific width for all nav items with text centered
+            .nav li a {
+            // specific width  154px
+            // specific height    114px
+            // text-align: center
+            }
+
+            .nav li a.active {
+            // another specific width 170
+            // another specific height 165
+            // other css
+            }
+
+    
+banner:
+    add image as background:
+        use property background-size cover
