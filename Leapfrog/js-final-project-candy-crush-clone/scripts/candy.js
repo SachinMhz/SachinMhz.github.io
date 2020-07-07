f7bfc978b690faa4b8397e6d9092e1b9ba69cd2c
SachinMhz <sachin.mhz.sm@gmail.com>
2020-07-07 22:41:55 +0545

    add images functions

--- Leapfrog/js-final-project-candy-crush-clone/scripts/candy.js
+++ Leapfrog/js-final-project-candy-crush-clone/scripts/candy.js
@@ -1,43 +1,86 @@
-import { CANVAS, CTX, CANDY_HEIGHT, CANDY_WIDTH } from "./constants.js";
-import { drawRectContext, getRandomColor, swapArray } from "./helperFunc.js";
+import {
+  CANVAS,
+  CTX,
+  CANDY_HEIGHT,
+  CANDY_WIDTH,
+  RED,
+  BLUE,
+  GREEN,
+  YELLOW,
+  ORANGE,
+  PURPLE,
+} from "./constants.js";
+import {
+  drawRectContext,
+  getRandomColor,
+  swapArray,
+  drawImageContext,
+} from "./helperFunc.js";
 
 export default function Candy(game, x, y, color, id) {
   this.game = game;
-  this.realX = x;
-  this.realY = y;
-  this.x = x;
-  this.y = y;
-  this.id = id;
+  this.realX = x * CANDY_WIDTH;
+  this.realY = y * CANDY_HEIGHT;
+  this.x = x * CANDY_WIDTH;
+  this.y = y * CANDY_HEIGHT;
   this.width = CANDY_WIDTH;
   this.height = CANDY_HEIGHT;
   this.color = color;
+  this.id = id;
   this.isDragging = false;
   this.zIndex = 0;
   this.dragDirection = "center";
   this.moveAboveCandy = false;
 
   this.draw = () => {
-    drawRectContext(CTX, this.x, this.y, this.width, this.height, this.color);
+    switch (this.color) {
+      case "red":
+        drawImageContext(CTX, RED, this.x, this.y, this.width, this.height);
+        break;
+      case "blue":
+        drawImageContext(CTX, BLUE, this.x, this.y, this.width, this.height);
+        break;
+      case "green":
+        drawImageContext(CTX, GREEN, this.x, this.y, this.width, this.height);
+        break;
+      case "yellow":
+        drawImageContext(CTX, YELLOW, this.x, this.y, this.width, this.height);
+        break;
+      case "orange":
+        drawImageContext(CTX, ORANGE, this.x, this.y, this.width, this.height);
+        break;
+      case "purple":
+        drawImageContext(CTX, PURPLE, this.x, this.y, this.width, this.height);
+        break;
+      default:
+        drawImageContext(CTX, PURPLE, this.x, this.y, this.width, this.height);
+    }
+    //drawRectContext(CTX, this.x, this.y, this.width, this.height, this.color);
   };
-
-  // this.moveDown = () => {
-  //   if (this.isMoveDown) {
-  //     this.y = +60;
-  //     this.realY += 60;
-  //     this.isMoveDown = false;
-  //   }
-  // };
-
-  // this.destroyed = () => {
-  //   if (this.isDestroyed) {
-  //     //this.isDestroyed = false;
-  //     if (this.game.candies[this.id - 10]) {
-  //       swapArray(
-  //         this.game.candies,
-  //         this.game.candies[this.id].id,
-  //         this.game.candies[this.id - 10].id
-  //       );
-  //     }
-  //   }
-  // };
 }
+
+/*
+switch (this.color) {
+      case "red":
+        drawImageContext(CTX, RED, this.x, this.y, this.width, this.height);
+        break;
+      case "blue":
+        drawImageContext(CTX, BLUE, this.x, this.y, this.width, this.height);
+        break;
+      case "green":
+        drawImageContext(CTX, GREEN, this.x, this.y, this.width, this.height);
+        break;
+      case "yellow":
+        drawImageContext(CTX, YELLOW, this.x, this.y, this.width, this.height);
+        break;
+      case "orange":
+        drawImageContext(CTX, ORANGE, this.x, this.y, this.width, this.height);
+        break;
+      case "purple":
+        drawImageContext(CTX, PURPLE, this.x, this.y, this.width, this.height);
+        break;
+      default:
+        drawImageContext(CTX, PURPLE, this.x, this.y, this.width, this.height);
+    }
+
+    */
--- Leapfrog/js-final-project-candy-crush-clone/scripts/constants.js
+++ Leapfrog/js-final-project-candy-crush-clone/scripts/constants.js
@@ -12,7 +12,12 @@ export const CANDY_WIDTH = 60;
 export const CANDY_HEIGHT = 60;
 
 //declaring image constants
-//export const bird1_IMG = getImage("./images/bird1.png");
+export const BLUE = getImage("./images/blue-candy.png");
+export const GREEN = getImage("./images/green-candy.png");
+export const RED = getImage("./images/red-candy.png");
+export const YELLOW = getImage("./images/yellow-candy.png");
+export const PURPLE = getImage("./images/purple-candy.png");
+export const ORANGE = getImage("./images/orange-candy.png");
 
 //get audio files
 // export const pointAudio = getAudio("./audios/point.mp3");
