4f611afc5dbb6b7316ca8d91820cef39111bbd6d
SachinMhz <sachin.mhz.sm@gmail.com>
2020-07-07 22:41:40 +0545

    switch to tile map concept

--- Leapfrog/js-final-project-candy-crush-clone/scripts/index.js
+++ Leapfrog/js-final-project-candy-crush-clone/scripts/index.js
@@ -8,93 +8,108 @@ import {
   sortCandies,
   swapArray,
 } from "./helperFunc.js";
+import { randomInt } from "./helperFunc.js";
 
 var game = new Game();
-
-function init() {
+var map = [];
+for (let i = 0; i < 10; i++) {
+  let row = [];
+  for (let j = 0; j < 10; j++) {
+    row.push(randomInt(1, 6));
+  }
+  map.push(row);
+}
+function changeCandiesList() {
+  game.candies = [];
   let id = 0;
   for (let i = 0; i < 10; i++) {
+    let row = [];
     for (let j = 0; j < 10; j++) {
-      let color = getRandomColor();
       //params gameObject, xPos, yPos, color, id
-      var candy = new Candy(game, j * 60, i * 60, color, id);
+      let color = null;
+      switch (map[i][j]) {
+        case 1:
+          color = "red";
+          break;
+        case 2:
+          color = "blue";
+          break;
+        case 3:
+          color = "green";
+          break;
+        case 4:
+          color = "yellow";
+          break;
+        case 5:
+          color = "orange";
+          break;
+        default:
+          color = "black";
+      }
+      var candy = new Candy(game, j, i, color, id);
+      row.push(candy);
       id++;
-      game.candies.push(candy);
     }
+    game.candies.push(row);
   }
 }
-
-init();
+changeCandiesList();
 
 function draw() {
   CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
   checkThreeRow();
-  checkColumnForThree();
-  //moveDown();  //uncomment for move down
-  game.candies.forEach((candy) => {
-
-    candy.draw();
+  checkThreeColumn();
+  moveDown();
+  game.candies.forEach((candiesRow) => {
+    candiesRow.forEach((candy) => {
+      candy.draw();
+    });
   });
   requestAnimationFrame(draw);
 }
-
-
 draw();
 
 function checkThreeRow() {
-  for (let i = 0; i < game.candies.length - 2; i++) {
-    let rowOfThree = [i, i + 1, i + 2];
-    let color = game.candies[i].color;
-    const notValid = [
-      8,
-      9,
-      18,
-      19,
-      28,
-      29,
-      38,
-      39,
-      48,
-      49,
-      58,
-      59,
-      69,
-      69,
-      78,
-      79,
-      88,
-      89,
-      98,
-      99,
-    ];
-    if (notValid.includes(i)) continue;
-
-    if (rowOfThree.every((index) => game.candies[index].color === color)) {
-      rowOfThree.forEach((index) => {
-        game.candies[index].color = "black";
-        game.candies[index].moveAboveCandy = true;
-      });
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map.length - 2; j++) {
+      let rowOfThree = [j, j + 1, j + 2];
+      let value = map[i][j];
+      if (rowOfThree.every((index) => map[i][index] === value)) {
+        rowOfThree.forEach((index) => {
+          map[i][index] = 0;
+          changeCandiesList();
+        });
+      }
     }
   }
 }
 
-function checkColumnForThree() {
-  for (let i = 0; i < 80; i++) {
-    let columnOfThree = [i, i + 10, i + 10 * 2];
-    let color = game.candies[i].color;
-
-    if (columnOfThree.every((index) => game.candies[index].color === color)) {
-      columnOfThree.forEach((index) => {
-        game.candies[index].color = "black";
-      });
+function checkThreeColumn() {
+  for (let i = 0; i < map.length - 2; i++) {
+    for (let j = 0; j < map.length; j++) {
+      let columnOfThree = [i, i + 1, i + 2];
+      let value = map[i][j];
+      if (columnOfThree.every((index) => map[index][j] === value)) {
+        columnOfThree.forEach((index) => {
+          map[index][j] = 0;
+          changeCandiesList();
+        });
+      }
     }
   }
 }
 
 function moveDown() {
-  for (let i = 0; i < 90; i++) {
-    if (game.candies[i + 10].moveAboveCandy) {
-      swapArray(game.candies, i, i + 10);
+  for (let row = map.length - 2; row >= 0; row--) {
+    for (let col = map[0].length; col >= 0; col--) {
+      if (map[row + 1][col] === 0) {
+        swapArray(map, row + 1, col, row, col);
+        changeCandiesList();
+      }
+      if (map[0][col] === 0) {
+        map[row][col] = randomInt(1, 6);
+        changeCandiesList();
+      }
     }
   }
 }
@@ -104,12 +119,12 @@ CANVAS.addEventListener("mousedown", (e) => {
     x: e.offsetX,
     y: e.offsetY,
   };
-
-  game.candies.forEach((candy) => {
-    if (isPointInsideRect(mouse, candy)) {
-      candy.isDragging = true;
-      candy.zIndex = 1;
-    }
+  game.candies.forEach((candyList) => {
+    candyList.forEach((candy) => {
+      if (isPointInsideRect(mouse, candy)) {
+        candy.isDraggable = true;
+      }
+    });
   });
 });
 
@@ -118,28 +133,36 @@ CANVAS.addEventListener("mousemove", (e) => {
     x: e.offsetX,
     y: e.offsetY,
   };
-  for (let i = 0; i < game.candies.length; i++) {
-    if (game.candies[i].isDragging && isDragLimit(mouse, game.candies[i])) {
-      if (!isDragLimit(mouse, game.candies[i])) {
-        game.candies[i].x = game.candies[i].realX;
-        game.candies[i].y = game.candies[i].realY;
-        game.candies[i].isDragging = false;
-      } else {
-        game.candies[i].dragDirection = isDragLimit(mouse, game.candies[i]);
-        game.candies[i].x = mouse.x - game.candies[i].width / 2;
-        game.candies[i].y = mouse.y - game.candies[i].height / 2;
-        if (game.candies[i].dragDirection === "right") {
-          swapArray(game.candies, i, i + 1);
-        } else if (game.candies[i].dragDirection === "left") {
-          swapArray(game.candies, i, i - 1);
-        } else if (game.candies[i].dragDirection === "up") {
-          swapArray(game.candies, i, i - 10);
-        } else if (game.candies[i].dragDirection === "down") {
-          swapArray(game.candies, i, i + 10);
+
+  game.candies.forEach((candyList) => {
+    candyList.forEach((candy) => {
+      if (candy.isDraggable && isDragLimit(mouse, candy)) {
+        candy.x = mouse.x - candy.width / 2;
+        candy.y = mouse.y - candy.height / 2;
+
+        let direction = isDragLimit(mouse, candy);
+        let row = Math.floor(candy.id / 10);
+        let col = candy.id % 10;
+
+        if (direction === "right") {
+          swapArray(map, row, col, row, col + 1);
+          changeCandiesList();
+        } else if (direction === "left") {
+          swapArray(map, row, col, row, col - 1);
+          changeCandiesList();
+        } else if (direction === "up") {
+          swapArray(map, row, col, row - 1, col);
+          changeCandiesList();
+        } else if (direction === "down") {
+          swapArray(map, row, col, row + 1, col);
+          changeCandiesList();
         }
+      } else {
+        candy.x = candy.realX;
+        candy.y = candy.realY;
       }
-    }
-  }
+    });
+  });
 });
 
 CANVAS.addEventListener("mouseup", (e) => {
@@ -147,12 +170,9 @@ CANVAS.addEventListener("mouseup", (e) => {
     x: e.offsetX,
     y: e.offsetY,
   };
-  for (let i = 0; i < 100; i++) {
-    if (isPointInsideRect(mouse, game.candies[i])) {
-      game.candies[i].isDragging = false;
-      game.candies[i].zIndex = 0;
-      game.candies[i].x = game.candies[i].realX;
-      game.candies[i].y = game.candies[i].realY;
-    }
-  }
+  game.candies.forEach((candyList) => {
+    candyList.forEach((candy) => {
+      candy.isDraggable = false;
+    });
+  });
 });
