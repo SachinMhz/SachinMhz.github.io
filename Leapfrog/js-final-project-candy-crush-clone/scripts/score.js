7ee20e863043d1acad25c2733cd9c14ebf939249
SachinMhz <sachin.mhz.sm@gmail.com>
2020-07-09 22:09:03 +0545

    add functionality for displaying scores and moves

---
+++ Leapfrog/js-final-project-candy-crush-clone/scripts/score.js
@@ -0,0 +1,17 @@
+export default function Score(game) {
+  this.game = game;
+  this.score = 0;
+  this.highScore = localStorage.getItem("highScore")
+    ? localStorage.getItem("highScore")
+    : 0;
+  this.moves = 50;
+  this.scoreText = document.getElementById("score");
+  this.moveText = document.getElementById("moves");
+  this.highScoreText = document.getElementById("highScore");
+
+  this.draw = () => {
+    this.scoreText.innerHTML = "Score: " + this.score;
+    this.moveText.innerHTML = "Moves: " + this.moves;
+    this.highScoreText.innerHTML = "High Score: " + this.highScore;
+  };
+}
