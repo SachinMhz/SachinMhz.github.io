ac6acfc63b2d87210770d3f75a095ef7b327a303
SachinMhz <sachin.mhz.sm@gmail.com>
2020-07-18 23:14:57 +0545

    add audio preloader

--- Leapfrog/js-final-project-candy-crush-clone/scripts/preloader.js
+++ Leapfrog/js-final-project-candy-crush-clone/scripts/preloader.js
@@ -1,5 +1,5 @@
 import { CTX, CANVAS } from "./constants.js";
-//import { addAudio } from "./audio.js";
+//import { addAudio } from './audio.js';
 
 export default function Preloader() {
   this.images = [
@@ -37,14 +37,51 @@ export default function Preloader() {
     "./images/bomb",
 
     "./images/noBG",
-    "./images/bg1",
-    "./images/bg1-selected",
-    "./images/bg2",
-    "./images/bg3",
-    "./images/bg4",
+    "./images/CandyBG",
+    "./images/CandyBG-selected",
+
+    "./images/back",
+    "./images/back-selected",
+    "./images/level1",
+    "./images/level1-selected",
+    "./images/level2",
+    "./images/level2-selected",
+    "./images/level3",
+    "./images/level3-selected",
+    "./images/level4",
+    "./images/level4-selected",
+    "./images/level5",
+    "./images/level5-selected",
+    "./images/menuBtn",
+    "./images/menuBtn-selected",
+    "./images/nextBtn",
+    "./images/nextBtn-selected",
+    "./images/playBtn",
+    "./images/playBtn-selected",
+    "./images/retryBtn",
+    "./images/retryBtn-selected",
+
+    "./images/startBG",
+    "./images/levelBG",
+    "./images/scoreBG",
+    "./images/gameOverBG",
+    "./images/gameCompleteBG",
   ];
 
-  this.audio = ["./audios/bg_music.mp3", "./audios/colour_bomb_blast.mp3"];
+  this.audio = [
+    "./audios/bg_music.mp3",
+    "./audios/colour_bomb_blast.mp3",
+    "./audios/colour_bomb_created.mp3",
+    "./audios/combo_sound.mp3",
+    "./audios/level_completed.mp3",
+    "./audios/level_failed.mp3",
+    "./audios/line_blast.mp3",
+    "./audios/line_blast2.mp3",
+    "./audios/packet_blast.mp3",
+    "./audios/packet_candy_created.mp3",
+    "./audios/striped_candy_created.mp3",
+    "./audios/swap.mp3",
+  ];
   this.loadedImages = 0;
   this.loadedAudio = 0;
 
@@ -56,6 +93,7 @@ export default function Preloader() {
       CTX.fill();
       CTX.fillStyle = "white";
       CTX.fillText("Loading Images ...", 300, 300);
+
       var img = new Image();
       img.src = this.images[i] + ".png";
       img.onload = () => {
@@ -75,9 +113,9 @@ export default function Preloader() {
       CTX.fill();
       CTX.fillStyle = "white";
       CTX.fillText("Loading Sound ...", 300, 300);
+
       var audio = new Audio();
       audio.src = this.audio[i];
-
       audio.addEventListener("canplay", () => {
         this.loadedAudio += 1;
         if (this.loadedAudio == this.audio.length - 1) {
