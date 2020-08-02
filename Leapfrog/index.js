e4986350957e324cfabe5916901a6fbb4aedccec
SachinMhz <sachin.mhz.sm@gmail.com>
2020-08-02 17:50:26 +0545

    update index table for hackerNews

--- Leapfrog/index.js
+++ Leapfrog/index.js
@@ -1,12 +1,13 @@
 var table = document.getElementById("main-table");
 
-class TableRow {
-  constructor(heading, path, parentElement) {
+class TableData {
+  constructor(heading, codeLink, demoLink) {
     self = this;
     this.heading = heading;
-    this.path = path;
+    this.codeLink = codeLink;
+    this.demoLink = demoLink;
     this.element = this.createElement();
-    this.parentElement = parentElement;
+    this.parentElement = table;
     this.render(this.parentElement);
   }
 
@@ -24,16 +25,9 @@ class TableRow {
     demoCol.appendChild(demoColLink);
     titleCol.innerHTML = this.heading;
     codeColLink.innerHTML = this.heading + " - Code";
-    codeColLink.setAttribute(
-      "href",
-      "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/" +
-        this.path
-    );
+    codeColLink.setAttribute("href", this.codeLink);
     demoColLink.innerHTML = this.heading + " - Demo";
-    demoColLink.setAttribute(
-      "href",
-      "https://sachinmhz.github.io/Leapfrog/" + this.path
-    );
+    demoColLink.setAttribute("href", this.demoLink);
 
     row.classList += "table-row";
     return row;
@@ -48,103 +42,122 @@ class TableRow {
   }
 }
 
-var tr2 = new TableRow("Git Assignment 1", "", table);
+var tr2 = new TableData(
+  "Git Assignment 1",
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/",
+  "https://sachinmhz.github.io/Leapfrog/"
+);
 
-var tr3 = new TableRow(
+var tr3 = new TableData(
   "Design Assignment 1 A-home-search.jpg",
-  "TuTriangle/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/TuTriangle/",
+  "https://sachinmhz.github.io/Leapfrog/TuTriangle/"
 );
 
-var tr4 = new TableRow(
+var tr4 = new TableData(
   "Design Assignment 2 A-home-search-responsive.jpg",
-  "TuTriangleResponsive/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/TuTriangleResponsive/",
+  "https://sachinmhz.github.io/Leapfrog/TuTriangleResponsive/"
 );
 
-var tr5 = new TableRow(
+var tr5 = new TableData(
   "Design Final Project: Responsive",
-  "design_final_project/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/design_final_project/",
+  "https://sachinmhz.github.io/Leapfrog/design_final_project/"
 );
 
-var tr6 = new TableRow(
+var tr6 = new TableData(
   "JS-Exercises: 2-7",
-  "JS_assignment/JS_Exercises_2-7/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/JS_assignment/JS_Exercises_2-7/",
+  "https://sachinmhz.github.io/Leapfrog/JS_assignment/JS_Exercises_2-7/"
 );
 
-var tr7 = new TableRow("JS-Exercise: 8", "JS_assignment/JS_Exercise_8/", table);
+var tr7 = new TableData(
+  "JS-Exercise: 8",
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/JS_assignment/JS_Exercise_8/",
+  "https://sachinmhz.github.io/Leapfrog/JS_assignment/JS_Exercise_8/"
+);
 
-var tr7 = new TableRow("JS-Exercise: 9", "JS_assignment/JS_Exercise_9/", table);
+var tr7 = new TableData(
+  "JS-Exercise: 9",
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/JS_assignment/JS_Exercise_9/",
+  "https://sachinmhz.github.io/Leapfrog/JS_assignment/JS_Exercise_9/"
+);
 
-var tr8 = new TableRow(
+var tr8 = new TableData(
   "JS-Exercise: 8 - using OOP",
-  "JS_assignment/JS_Exercise_using_OOP/JS_Exercise_8/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/JS_assignment/JS_Exercise_using_OOP/JS_Exercise_8/",
+  "https://sachinmhz.github.io/Leapfrog/JS_assignment/JS_Exercise_using_OOP/JS_Exercise_8/"
 );
 
-var tr9 = new TableRow(
+var tr9 = new TableData(
   "JS-Exercise: 9 - using OOP",
-  "JS_assignment/JS_Exercise_using_OOP/JS_Exercise_9/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/JS_assignment/JS_Exercise_using_OOP/JS_Exercise_9/",
+  "https://sachinmhz.github.io/Leapfrog/JS_assignment/JS_Exercise_using_OOP/JS_Exercise_9/"
 );
 
-var tr10 = new TableRow(
+var tr10 = new TableData(
   "JS Assignment 1:  Image Slider",
-  "js-image-slider/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-image-slider/",
+  "https://sachinmhz.github.io/Leapfrog/js-image-slider/"
 );
 
-var tr11 = new TableRow(
+var tr11 = new TableData(
   "JS Assignment 2:  Image Slider with Hold Times",
-  "js-image-slider-hold/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-image-slider-hold/",
+  "https://sachinmhz.github.io/Leapfrog/js-image-slider-hold/"
 );
 
-var tr12 = new TableRow(
+var tr12 = new TableData(
   "JS Assignment 3:  Ball Collision and Ant Smasher",
-  "js-box-collision-and-ant-smasher/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-box-collision-and-ant-smasher/",
+  "https://sachinmhz.github.io/Leapfrog/js-box-collision-and-ant-smasher/"
 );
 
-var tr13 = new TableRow(
+var tr13 = new TableData(
   "JS Assignment 4:  Car Lane Game with Bullets",
-  "js-car-lane-with-bullets/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-car-lane-with-bullets/",
+  "https://sachinmhz.github.io/Leapfrog/js-car-lane-with-bullets/"
 );
 
-var tr14 = new TableRow(
+var tr14 = new TableData(
   "JS Assignment 5:  Flappy Bird",
-  "js-flappy-bird/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-flappy-bird/",
+  "https://sachinmhz.github.io/Leapfrog/js-flappy-bird/"
 );
 
-var tr15 = new TableRow("JS Assignment 6: Helix Animation", "js-helix/", table);
+var tr15 = new TableData(
+  "JS Assignment 6: Helix Animation",
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-helix/",
+  "https://sachinmhz.github.io/Leapfrog/js-helix/"
+);
 
-var tr16 = new TableRow(
+var tr16 = new TableData(
   "JS Final Project: Candy Crush Clone",
-  "js-final-project-candy-crush-clone/",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/js-final-project-candy-crush-clone/",
+  "https://sachinmhz.github.io/Leapfrog/js-final-project-candy-crush-clone/"
 );
 
-
-var tr17 = new TableRow(
+var tr17 = new TableData(
   "Advance Design: BEM",
-  "advance-design/BEM-assignment",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/dvance-design/BEM-assignment",
+  "https://sachinmhz.github.io/Leapfrog/advance-design/BEM-assignment"
 );
 
-
-var tr18 = new TableRow(
+var tr18 = new TableData(
   "Advance Design: SASS",
-  "advance-design/SAAS-assignment/dist",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/advance-design/SAAS-assignment/dist",
+  "https://sachinmhz.github.io/Leapfrog/advance-design/SAAS-assignment/dist"
 );
 
-
-var tr19 = new TableRow(
+var tr19 = new TableData(
   "React",
-  "react/Todo",
-  table
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/react/Todo",
+  "https://sachinmhz.github.io/Leapfrog/react/Todo"
+);
+
+var tr20 = new TableData(
+  "HackerNews",
+  "https://github.com/SachinMhz/SachinMhz.github.io/tree/master/Leapfrog/react/hacker-news",
+  "https://sachinmhz.github.io/hacker-news/"
 );
