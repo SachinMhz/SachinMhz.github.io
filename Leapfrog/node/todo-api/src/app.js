e870cf49dcfa057fc965d1fcb5b9afca6c3fb230
SachinMhz <sachin.mhz.sm@gmail.com>
2020-08-18 16:01:14 +0545

    add comments

--- Leapfrog/node/todo-api/src/app.js
+++ Leapfrog/node/todo-api/src/app.js
@@ -1,17 +1,18 @@
+//importing modules
 const express = require("express");
 const cors = require("cors");
-
-const routes = require("./routes");
-const logger = require("./utils/logger");
-const errorHandler = require("./middlewares/errorHandler");
-const pool = require("./db");
 const EXPRESS_VALIDATOR = require('express-validator');
+
+//importing files
+const routes = require("./routes");
 require("./env");
 
+//initializing app
 const app = express();
 app.set("host", process.env.SERVER_HOST);
 app.set("port", process.env.SERVER_PORT);
 
+//middlewares
 app.use(EXPRESS_VALIDATOR());
 app.use(cors());
 app.use(express.json());
@@ -19,15 +20,18 @@ app.use(express.json());
 // API Routes
 app.use("/api", routes);
 
+//for handling home paths
 app.get("/", (req, res) =>
   res.json({ success: true, msg: "API is ready... use valid API" })
 );
 
+//for handling home paths
 app.use((req, res) => res.json({ status: 404, msg: "Route not found" }));
 
 // app.use(errorHandler.genericErrorHandler);
 // app.use(errorHandler.notFound);
 
+//for handling errors - error-handling-middleware
 app.use((err, req, res, next) => {
   res.status(err.status || 400).json({
     msg: err.msg || err,
@@ -35,6 +39,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+//listening port
 app.listen(app.get("port"), app.get("host"), () => {
   console.log(`Server running on http://${app.get("host")}:${app.get("port")}`);
 });
